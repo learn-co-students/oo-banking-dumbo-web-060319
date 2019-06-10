e91ad8891458d8e524730f5ed61f70b4d770a8b9
TL <nbotapp1@gmail.com>
2019-06-09 21:40:50 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
+	attr_accessor :status
+	attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+  	@sender = sender
+  	@receiver = receiver
+  	@amount = amount
+  	@status = "pending"
+  end
+
+  def valid?
+  	if sender.valid? && receiver.valid?
+  		return true
+  	end
+  	false
+  end
+
+  def execute_transaction
+  	if valid? && sender.balance > amount && self.status == 'pending'
+  		sender.balance -= amount
+  		receiver.balance += amount
+  		@status = 'complete'
+  	else
+  		@status = 'rejected'
+  		"Transaction rejected. Please check your account balance."
+  	end
+  end
+
+  def reverse_transfer
+  	if valid? && receiver.balance > amount && self.status == 'complete'
+  		sender.balance += amount
+  		receiver.balance -= amount
+  		@status = 'reversed'
+  	end
+  end
+
 end
